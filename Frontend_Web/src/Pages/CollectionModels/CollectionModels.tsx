d2a5a78ef7477bd340aa403d7772d3b71560d414
IT22052124 <IT22052124@my.sliit.lk>
2024-10-17 01:58:18 +0530

    update

---
+++ Frontend_Web/src/Pages/CollectionModels/CollectionModels.tsx
@@ -0,0 +1,7 @@
+export const CollectionModels = () => {
+  return (
+    <div>
+      <h1>CollectionModels</h1>
+    </div>
+  );
+};
---
+++ Frontend_Web/src/controllers/CollectionModelController.tsx
@@ -0,0 +1,96 @@
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import { db } from "@/storage/firebase";
+
+// Add new collection model
+export const addCollectionModel = async (collectionModelData) => {
+  try {
+    const docRef = await addDoc(collection(db, "collectionModels"), {
+      ...collectionModelData,
+      createdAt: serverTimestamp(),
+    });
+    console.log("Collection Model added with ID: ", docRef.id);
+    return docRef;
+  } catch (e) {
+    console.error("Error adding collection model: ", e);
+    throw e;
+  }
+};
+
+// Get all collection models
+export const getCollectionModels = async () => {
+  try {
+    const collectionModelCollection = collection(db, "collectionModels");
+    const collectionModelSnapshot = await getDocs(collectionModelCollection);
+    const collectionModels = collectionModelSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    console.log("Collection models retrieved successfully:", collectionModels);
+    return collectionModels;
+  } catch (error) {
+    console.error("Error retrieving collection models:", error);
+    throw new Error("Failed to fetch collection models");
+  }
+};
+
+// Update a collection model by ID
+export const updateCollectionModel = async (id, updatedCollectionModelData) => {
+  try {
+    const collectionModelRef = doc(db, "collectionModels", id);
+    await updateDoc(collectionModelRef, {
+      ...updatedCollectionModelData,
+      updatedAt: serverTimestamp(),
+    });
+    console.log("Collection model updated with ID: ", id);
+  } catch (e) {
+    console.error("Error updating collection model: ", e);
+    throw e;
+  }
+};
+
+// Delete a collection model by ID
+export const deleteCollectionModel = async (id) => {
+  try {
+    const collectionModelRef = doc(db, "collectionModels", id);
+    await deleteDoc(collectionModelRef);
+    console.log("Collection model deleted with ID: ", id);
+  } catch (e) {
+    console.error("Error deleting collection model: ", e);
+    throw e;
+  }
+};
+
+// Get a collection model by ID
+export const getCollectionModelById = async (id) => {
+  try {
+    const collectionModelRef = doc(db, "collectionModels", id);
+    const collectionModelDoc = await getDoc(collectionModelRef);
+
+    if (collectionModelDoc.exists()) {
+      const collectionModelData = {
+        id: collectionModelDoc.id,
+        ...collectionModelDoc.data(),
+      };
+      console.log(
+        "Collection model retrieved successfully:",
+        collectionModelData
+      );
+      return collectionModelData;
+    } else {
+      console.log("No such document!");
+      throw new Error("Collection model not found");
+    }
+  } catch (error) {
+    console.error("Error retrieving collection model:", error);
+    throw new Error("Failed to fetch collection model by ID");
+  }
+};
