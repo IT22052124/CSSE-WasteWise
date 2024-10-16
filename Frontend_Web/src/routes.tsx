330183eec4a5c87a87dd304382ed9a16668ec7b2
IT22052124 <IT22052124@my.sliit.lk>
2024-10-17 01:41:32 +0530

    Update routes.tsx

--- Frontend_Web/src/routes.tsx
+++ Frontend_Web/src/routes.tsx
@@ -10,9 +10,15 @@ import { Home, Profile, Tables, Notifications } from "@/Pages/dashboard";
 import { SignIn, SignUp } from "@/Pages/auth";
 import { AddBin } from "./Pages/Bins/AddBin";
 
-import { WasteTypes } from "./Pages/Waste/WasteTypes";
-import { AddWasteType } from "./Pages/Waste/AddWasteType";
-import { UpdateWasteType } from "./Pages/Waste/UpdateWasteType";
+//Waste Types Elements
+import { WasteTypes } from "./Pages/WasteTypes/WasteTypes";
+import { AddWasteType } from "./Pages/WasteTypes/AddWasteType";
+import { UpdateWasteType } from "./Pages/WasteTypes/UpdateWasteType";
+
+//Collection Model Elements
+import { AddCollectionModel } from "./Pages/CollectionModels/AddCollectionModel";
+import { CollectionModels } from "./Pages/CollectionModels/CollectionModels";
+import { UpdateCollectionModel } from "./Pages/CollectionModels/UpdateCollectionModel";
 
 const icon = {
   className: "w-5 h-5 text-inherit",
@@ -53,6 +59,42 @@ export const routes = [
         element: <UpdateWasteType />,
         inSidebar: false,
         relatedPaths: ["/wastetypes", "/addwastetypes", "/updatewastetypes"],
+      },
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "Waste Collection Models",
+        path: "/collectionmodels",
+        element: <CollectionModels />,
+        inSidebar: true,
+        relatedPaths: [
+          "/collectionmodels",
+          "/addcollectionmodel",
+          "/updatecollectionmodel",
+        ],
+      },
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "Add Waste Collection Models",
+        path: "/addcollectionmodel",
+        element: <AddCollectionModel />,
+        inSidebar: false,
+        relatedPaths: [
+          "/collectionmodels",
+          "/addcollectionmodel",
+          "/updatecollectionmodel",
+        ],
+      },
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "Update Waste Collection Models",
+        path: "/updatecollectionmodel/:id",
+        element: <UpdateCollectionModel />,
+        inSidebar: false,
+        relatedPaths: [
+          "/collectionmodels",
+          "/addcollectionmodel",
+          "/updatecollectionmodel",
+        ],
       },
       {
         icon: <UserCircleIcon {...icon} />,
