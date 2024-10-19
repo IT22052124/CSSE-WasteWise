1f482cd0d8c357eea67d2fecbef44bc1a5028c61
Faizal Amaan <123483157+Faizal2017@users.noreply.github.com>
2024-10-19 11:56:57 +0530

    q

--- Frontend_User/storage/firebase.tsx
+++ Frontend_User/storage/firebase.tsx
@@ -1,6 +1,27 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB5krGSrH9mO408AoaEzDAfWi4-FZk6Yes",
+//   authDomain: "frontend-web-e454c.firebaseapp.com",
+//   projectId: "frontend-web-e454c",
+//   storageBucket: "frontend-web-e454c.appspot.com",
+//   messagingSenderId: "477577416048",
+//   appId: "1:477577416048:web:a9acef3ba4e0058f9fd3b5",
+//   measurementId: "G-PVHD7MMLSV",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// const storage = getStorage(app);
+
+// export { db, storage };
+
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDA_7kcXGORyc8_Wp-HYBpWWyMdGybPx6U",
@@ -12,9 +33,7 @@ const firebaseConfig = {
   measurementId: "G-V1JYYL0Z17",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-//const storage = getStorage(app);
 
-export { db };
+export { db };
